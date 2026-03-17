0a7da1615fd35147a113343026eda701087893fb
Teddy Mapesa <teddygavin@gmail.com>
2026-03-17 16:03:40 +0300

    fixing deployment error

---
+++ server.js
@@ -0,0 +1,6 @@
+const app = require('./api/index');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server running on;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 7               ; http://localhost:${port}`);
+});   55
